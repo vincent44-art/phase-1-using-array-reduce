6471b1b823455619a071760ff8601b39550aa555
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-23 13:49:54 +0300

    commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
 
 // Code your solution here
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; // Example array of battery amounts
+
+const totalBatteries = batteryBatches.reduce((accumulator, batch) => {
+  return accumulator + batch;
+}, 0); // Initial value is 0
+
+console.log(totalBatteries); // Output: Sum of all batteries
